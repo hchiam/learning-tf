32f8083f4084348375f804871a0b70b34de27e71
hchiam <hchiam@users.noreply.github.com>
2023-03-02 00:53:35 -0700

    demo setup

--- csharp/README.md
+++ csharp/README.md
@@ -5,8 +5,22 @@
 https://github.com/SciSharp/TensorFlow.NET
 
 ```sh
-# once:
+# to set up a new C# project once:
 dotnet new console --framework net7.0
 # to run:
 dotnet run
 ```
+
+```sh
+dotnet add package TensorFlow.NET
+dotnet add package TensorFlow.Keras
+# install TensorFlow binary:
+  # CPU version
+dotnet add package SciSharp.TensorFlow.Redist
+  # GPU version (requires CUDA and cuDNN)
+# dotnet add package SciSharp.TensorFlow.Redist-Windows-GPU
+```
+
+https://scisharp.github.io/tensorflow-net-docs/#/essentials/introduction?id=c-example
+
+https://github.com/SciSharp/SciSharp-Stack-Examples/blob/master/src/TensorFlowNET.Examples/HelloWorld.cs
--- csharp/examples/Program.cs
+++ csharp/examples/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Linq;
+using Tensorflow;
+using Tensorflow.NumPy;
+using static Tensorflow.Binding;
+using static Tensorflow.KerasApi;
+
+namespace HelloWorld
+{
+    class NeuralNetXorKeras
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            
+            tf.enable_eager_execution();
+
+            // // np.array isn't working for me for some reason
+            var x = np.array(new float[,] { { 0, 0 }, { 0, 1 }, { 1, 0 }, { 1, 1 } });
+            var y = np.array(new float[,] { { 0 }, { 1 }, { 1 }, { 0 } });
+
+            var model = keras.Sequential();
+            model.add(keras.Input(2));
+            model.add(keras.layers.Dense(32, keras.activations.Relu));
+            model.add(keras.layers.Dense(1, keras.activations.Sigmoid));
+            model.compile(optimizer: keras.optimizers.Adam(),
+                loss: keras.losses.MeanSquaredError(),
+                new[] { "accuracy" });
+            model.fit(x, y, 1, 100);
+            model.evaluate(x, y);
+            Tensor result = model.predict(x, 4);
+            
+            Console.WriteLine(result.ToArray<float>() is [< 0.5f, > 0.5f, > 0.5f, < 0.5f]);
+        }
+    }
+}
