2a95b107196a3a56c26a97015c1b25034e470585
hchiam <hchiam@users.noreply.github.com>
2023-11-15 19:16:17 -0700

    got assets.zip to export

--- js/decision-forest-demo/index.tsx
+++ js/decision-forest-demo/index.tsx
@@ -11,8 +11,8 @@ Bun.serve({
     if (url.pathname.endsWith("/tfjs_model/group1-shard1of1.bin"))
       return new Response(Bun.file(import.meta.dir + "/tfjs_model/group1-shard1of1.bin"));
 
-    // if (url.pathname.endsWith("/tfjs_model/assets.zip"))
-    //   return new Response(Bun.file(import.meta.dir + "/tfjs_model/assets.zip"));
+    if (url.pathname.endsWith("/tfjs_model/assets.zip"))
+      return new Response(Bun.file(import.meta.dir + "/tfjs_model/assets.zip"));
 
     // all other routes
     return new Response("Hello!");
